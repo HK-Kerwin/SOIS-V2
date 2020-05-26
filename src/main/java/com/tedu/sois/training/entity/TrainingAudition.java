244ce33ddcb6ee7015286d3afccd7a0b506f197f
LYS <735622418@qq.com>
2020-05-26 09:02:46 +0800

    添加了新的业务对象

---
+++ src/main/java/com/tedu/sois/training/entity/TrainingAudition.java
@@ -0,0 +1,8 @@
+package com.tedu.sois.training.entity;
+
+/**
+ * 训练营试听单
+ *
+ */
+public class TrainingAudition {
+}
---
+++ src/main/java/com/tedu/sois/training/entity/TrainingDocking.java
@@ -0,0 +1,9 @@
+package com.tedu.sois.training.entity;
+
+import com.tedu.sois.common.pojo.BaseEntity;
+
+/**
+ * 训练营对接表
+ */
+public class TrainingDocking extends BaseEntity {
+}
